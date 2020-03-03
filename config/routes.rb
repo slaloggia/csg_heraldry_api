4a06a49309476c32b6376836a33e9f6f9759078b
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-03-02 21:10:03 -0600

    Add get coat_of_arms to heraldry resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :heraldries
-  resources :members
+  resources :heraldries do
+    get :coat_of_arms, on: :member
+  end
+  resources :members 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
