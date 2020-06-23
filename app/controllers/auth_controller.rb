7242c81385bd78c85f49d0165cdca5cf60c1d530
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-06-22 22:32:45 -0500

    Move admin password to a gitignored environment variable

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,7 @@
-ADMIN_PASSWORD = "S0ndhe1m4Lyfe"
 
 class AuthController < ApplicationController
     def create
-        if params[:password] == ADMIN_PASSWORD
+        if params[:password] == ENV["ADMIN_PASSWORD"]
             render json: {auth: "Authorized"}
         else
             render json: {auth: "Incorrect Password"}
